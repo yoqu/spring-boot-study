2b19a4d7ee84349b081fbd77790648d555cecd00
yoqu <wcjiang2@iflytek.com>
2017-05-05 14:27:45 +0800

    Initial commit

---
+++ src/main/java/org/yoqu/springboot/study/StudyApplication.java
@@ -0,0 +1,12 @@
+package org.yoqu.springboot.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyApplication.class, args);
+	}
+}
